591492ab12925c680c8d0468be81de34305b7eff
bhaskar18-cmd <pradipkarmakar561@gmail.com>
2026-03-11 00:01:43 +0530

    Update text in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div style={{padding:"40px", fontSize:"24px"}}>
-      QR Craft Website is Working 🚀
+    <div style={{padding: "40px", fontSize: "24px"}}>
+      QR Craft is working 🚀
     </div>
-  );
+  )
 }
