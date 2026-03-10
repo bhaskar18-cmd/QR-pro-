5ddcf98d9348a985674a0ddaac01af198fe6f31c
bhaskar18-cmd <pradipkarmakar561@gmail.com>
2026-03-11 00:09:08 +0530

    Fix quotes in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: '/QR-pro-/',   // IMPORTANT for GitHub Pages
-  plugins: [react()]
+  plugins: [react()],
+  base: "/QR-pro-/"
 })
